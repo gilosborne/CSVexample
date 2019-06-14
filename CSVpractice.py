64034b1603a66ec55558fbc73a07e72a8a2d7e5e
gilosbo <gilbert.osborne@gmail.com>
2019-06-14 11:16:36 -0700

    rename and filter examples. youtube link updated

--- CSVpractice.py
+++ CSVpractice.py
@@ -1,6 +1,12 @@
-#https://www.youtube.com/watch?v=cXP_i5-nTXg&list=WL&index=5&t=923s
+#https://youtu.be/cXP_i5-nTXg?t=1214
 #import numpy as np
 import pandas as pd
 
-content = pd.read_csv('revenue-profit.csv')
-print(content)
+content = pd.read_csv('revenue-profit.csv', sep = ";") #sep is what separates the columns
+print(content.head()) #head is only first 5 rows
+
+content = content.rename(columns={'no_data':'Year'}) #Rename column name
+print(content.head())
+
+singleYear = content[content.Year == 2012] #Only columns with the year == 2012
+print(singleYear)
